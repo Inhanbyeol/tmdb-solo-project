6e1b3452422d4534905cfe9402aca4129ed0fef9
Siho <siho8808@gmail.com>
2023-06-05 15:07:23 +0900

    siho

--- assets/detail.js
+++ assets/detail.js
@@ -5,17 +5,13 @@ document.addEventListener('DOMContentLoaded', function () {
     const urlParams = new URLSearchParams(window.location.search);
     const movieId = urlParams.get('id');
 
-    const response = await fetch(
-      `https://api.themoviedb.org/3/movie/${movieId}?language=ko`,
-      {
-        method: 'GET',
-        headers: {
-          accept: 'application/json',
-          Authorization:
-            'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ZTM4NjU1N2ZkM2UyZDM3Y2EzZTZmZmVkNDBmNGYwNiIsInN1YiI6IjY0NzBiYTgzYzVhZGE1MDBjMWEzNjk1MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.XdDRFvv0qvVj6vK9qJ7qA5P5fYyYBQeOdh1G5-IB5uA',
-        },
-      }
-    );
+    const response = await fetch(`https://api.themoviedb.org/3/movie/${movieId}?language=ko`, {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ZTM4NjU1N2ZkM2UyZDM3Y2EzZTZmZmVkNDBmNGYwNiIsInN1YiI6IjY0NzBiYTgzYzVhZGE1MDBjMWEzNjk1MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.XdDRFvv0qvVj6vK9qJ7qA5P5fYyYBQeOdh1G5-IB5uA',
+      },
+    });
 
     const movieDetails = await response.json();
     detail(movieDetails);
@@ -23,15 +19,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
   const detail = (data) => {
     console.log(data);
-    const {
-      id,
-      title,
-      poster_path,
-      vote_average,
-      overview,
-      release_date,
-      runtime,
-    } = data;
+    const { id, title, poster_path, vote_average, overview, release_date, runtime } = data;
     const genre1 = data.genres[0].name;
     document.title = `${title}`;
     // main.style.backgroundImage = `url('https://image.tmdb.org/t/p/w500/${poster_path}')`;
--- assets/main.js
+++ assets/main.js
@@ -7,17 +7,13 @@ const movielist = document.getElementById('movielist');
 //const변수는 호이스팅이 불가하여 상단에 선언
 const getMovie = async () => {
   //api 변수에 서버로 api를 요청함.
-  const api = await fetch(
-    'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1',
-    {
-      method: 'GET',
-      headers: {
-        accept: 'application/json',
-        Authorization:
-          'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ZTM4NjU1N2ZkM2UyZDM3Y2EzZTZmZmVkNDBmNGYwNiIsInN1YiI6IjY0NzBiYTgzYzVhZGE1MDBjMWEzNjk1MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.XdDRFvv0qvVj6vK9qJ7qA5P5fYyYBQeOdh1G5-IB5uA',
-      },
-    }
-  );
+  const api = await fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ZTM4NjU1N2ZkM2UyZDM3Y2EzZTZmZmVkNDBmNGYwNiIsInN1YiI6IjY0NzBiYTgzYzVhZGE1MDBjMWEzNjk1MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.XdDRFvv0qvVj6vK9qJ7qA5P5fYyYBQeOdh1G5-IB5uA',
+    },
+  });
   //api 변수에서 서버로 api를 요청한 결과를 json 객체 형식으로 변환하며, 변환된 결과 내 results라는 키 값의 데이터만 불러올 수 있도록 변수 선언 시 {} 괄호를 통해 개별 추출
   const { results } = await api.json();
 
@@ -39,9 +35,7 @@ const search = async () => {
   const result = await getMovie();
 
   //result에 filter를 통해 대소문자와 관계없이, title key값에서 target값을 포함하고있는 객체만 추출하여 새로운 배열을 생성함
-  const findResult = result.filter(
-    (x) => x.title.toUpperCase().indexOf(target.toUpperCase()) !== -1
-  );
+  const findResult = result.filter((x) => x.title.toUpperCase().indexOf(target.toUpperCase()) !== -1);
 
   //만일 검색 결과가 없을 경우 검색결과가 없음을 alert 노출
   if (findResult.length == 0) return alert('검색 결과가 없습니다.');
@@ -110,5 +104,5 @@ searchBtn.addEventListener('click', search);
 searchInput.addEventListener('keyup', searchKeyup);
 
 const showMovieDetails = (movieId) => {
-  window.location.href = `../detail.html?id=${movieId}`;
+  window.location.href = `assets/detail.html?id=${movieId}`;
 };
